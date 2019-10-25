3b595fc877d35c7cecf1e9a8096638a277b1cc07
Syizuril <syihabklayan@gmail.com>
2019-10-25 16:18:38 +0700

    first commit

---
+++ src/aplikasisewalapangan/AplikasiSewaLapangan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasisewalapangan;
+
+/**
+ *
+ * @author Syekh Syihabuddin AU
+ */
+public class AplikasiSewaLapangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
